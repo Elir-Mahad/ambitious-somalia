8615bad53693a3dee6239861e22cccd8ac98c4b3
elir-mahad <mmahad280@gmail.com>
2022-01-06 22:05:12 -0500

    Write out components that will be created and the elements in each component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,55 @@ function App() {
   return (
     <div className="App">
       <h1>Ambsom</h1>
+      {/*  
+      
+      1. Menu 
+        - Its on the left side. Probably react-burger-menu.
+        - 4 options : home, resources, philosophy, ecosystems
+        - Green button at the bottom
+
+      2. General Landing page 
+        - Three components: nordic, africa, world
+        - Each component contains multiple City boxes.
+          - One CityBox component should be created
+            and then the different images and city names
+            should be passed into it as props from
+            a file with data object. 
+        - Each city box contains :
+            - A map image
+            - Name of the city overlayed on the map.
+        
+      3. Keys accra landing page
+        - The map in the background is animated and fixed.
+        - On scroll, the text boxes move from the bottom to the middle of map.
+        - As the text boxes move up, the text is blurry when it not in the middel of the map.
+
+      4. Accra resources page 
+        - Text 
+        - button
+        - List of organizations
+        - Footer containing three columns:
+          - Ecosystems: list of city names
+          - Resources: List of resource types
+          - About: philosophy, privacy policy
+
+      5. Forum 
+        - One component with 9 divs. 
+        - Each div contains:
+          - background animation.
+          - question
+          - buttons that user can click on to respond to the question
+            - there is effect on the button when its clicked
+          - Some divs have a button with the text 'next' at the bottom of the page
+        - When use clicks on a button, the div dissapears and the next one appears.
+        - Similar to the task tracker app that i tried to create. 
+        - Last div contains 
+          - Congratulations message
+          - Input field to submit email address
+          - checkmark with text
+          - submit button with the text send
+
+      */}
     </div>
   );
 }
