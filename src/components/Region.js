ece44fd8f9bd438c5b7df5c8921f327886a79cbc
elir-mahad <mmahad280@gmail.com>
2022-02-02 13:57:17 -0500

    Add menu to the right side of landing page. Remove links from sidebar. Update css to match site menu

--- src/components/Region.js
+++ src/components/Region.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Somalia from "./regions/Somalia";
 import Somaliland from "./regions/Somaliland";
+import SideBar from "./SideBar";
 
 function Region() {
   return (
     <div className="landing">
-      <Somalia />
-      <Somaliland />
+      <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+      <div id="page-wrap">
+        <Somalia />
+        <Somaliland />
+      </div>
     </div>
   );
 }
